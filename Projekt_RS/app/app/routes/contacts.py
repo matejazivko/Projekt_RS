14a51518895cec3d45f97ba3806f2b590a891cbc
matejazivko <mateja.zivko100@gmail.com>
2025-03-23 22:16:01 +0100

    Delete Projekt_RS/app/app/routes directory

--- Projekt_RS/app/app/routes/contacts.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.models import Contact
-
-router = APIRouter()
-
-@router.post("/send")
-def contact_owner(contact_message: Contact):
-    return {"message": f"Poruka za kuću {contact_message.house_name} uspješno poslana"}
